b78c12493ee4d9b42525dd7e36dd5a65cd611266
Kirill Fedyakov <kfedyakov@easirun.de>
2021-01-20 10:30:19 +0100

    added Poll model and configured admin interface

--- api/polls/models.py
+++ api/polls/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from crum import get_current_request
 
-# Create your models here.
+
+class Poll(models.Model):
+    """
+    Poll Model \n
+    fields: question, created_by, pub_date\n
+    Description: \n
+        The model managed polls in db
+    """
+    question = models.CharField("Question", max_length=100)
+    created_by = models.ForeignKey(
+        User, verbose_name="Created by",
+        editable=False, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField("Published on", auto_now_add=True)
+    last_modified = models.DateTimeField("Last modified on", auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "polls"
+
+    def __str__(self):
+        return "{}".format(self.question)
+
+    def save(self, *args, **kwargs):
+        request = get_current_request()
+        self.created_by = request.user
+        super(Poll, self).save(*args, **kwargs)
